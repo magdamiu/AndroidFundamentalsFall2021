a1f4d80e96efcd687feb028168d2257ad84bb7dd
magdamiu <badita.magda@gmail.com>
2021-11-02 21:04:47 +0200

    first course - intro android

---
+++ app/src/main/java/com/magdamiu/androidfundamentalsfall2021/MainActivity.java
@@ -0,0 +1,17 @@
+package com.magdamiu.androidfundamentalsfall2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //TODO new element to be added
+    private int newElement = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
