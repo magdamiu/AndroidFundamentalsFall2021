efeff35d2a1861d8d6b463fd862cbd062b377d1f
magdamiu <badita.magda@gmail.com>
2021-11-04 21:00:56 +0200

    login functionality from Java code

--- app/src/main/java/com/magdamiu/androidfundamentalsfall2021/MainActivity.java
+++ app/src/main/java/com/magdamiu/androidfundamentalsfall2021/MainActivity.java
@@ -4,22 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String MAIN_ACTIVITY = "MainActivity";
 
-    //TODO new element to be added
-    private int newElement = 0;
-
+    private EditText usernameEditText, passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-        setContentView(R.layout.samples);
+        setContentView(R.layout.login);
 
         // tag and message
         Log.e(MAIN_ACTIVITY, "this is my first error log");
         Log.w(MAIN_ACTIVITY, "this is my first warning log");
+
+        usernameEditText = findViewById(R.id.username);
+        passwordEditText = findViewById(R.id.password);
+    }
+
+    // handling click on login button
+    public void loginOnClick(View view) {
+        if (usernameEditText != null & passwordEditText != null) {
+            String usernameValue = usernameEditText.getText().toString();
+            String passwordValue = passwordEditText.getText().toString();
+            if(passwordValue.length() == 0) {
+                passwordEditText.setError("Please add your password");
+            } else {
+                Toast.makeText(MainActivity.this, usernameValue + " " + passwordValue, Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Toast.makeText(MainActivity.this, "please try again later", Toast.LENGTH_LONG).show();
+        }
     }
 }
