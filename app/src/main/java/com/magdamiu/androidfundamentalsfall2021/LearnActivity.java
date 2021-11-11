90eff59001315fbec3cec5bfed389eef48eed6c4
magdamiu <badita.magda@gmail.com>
2021-11-11 20:42:25 +0200

    epxlicit and implicit intent

--- app/src/main/java/com/magdamiu/androidfundamentalsfall2021/LearnActivity.java
+++ app/src/main/java/com/magdamiu/androidfundamentalsfall2021/LearnActivity.java
@@ -2,8 +2,12 @@ package com.magdamiu.androidfundamentalsfall2021;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class LearnActivity extends AppCompatActivity {
     private static final String LEARN_ACTIVITY = "LearnActivity";
@@ -49,5 +53,31 @@ public class LearnActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.e(LEARN_ACTIVITY, "onDestroy");
+    }
+
+    public void openActivityOnClick(View view) {
+        // explicit intent => starts an activity defined by us, the developers
+        Intent cakesActivity = new Intent(LearnActivity.this, CakesActivity.class);
+        startActivity(cakesActivity);
+    }
+
+
+    public void openCallActivityOnClick(View view) {
+        // implicit intent => starts an activity defined in the system
+        Intent callActivity = new Intent(Intent.ACTION_DIAL);
+        if(callActivity.resolveActivity(getPackageManager()) != null) {
+            callActivity.setData(Uri.parse("tel:077777777"));
+            startActivity(callActivity);
+        } else {
+            Toast.makeText(LearnActivity.this, "This action in not available on this device", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    public void openWebPageOnClick(View view) {
+        // implicit intent => starts an activity defined in the system
+        Intent openWebPage = new Intent(Intent.ACTION_VIEW);
+        openWebPage.setData(Uri.parse("https://developer.android.com/guide/components/intents-common#Email"));
+        startActivity(openWebPage);
     }
 }
